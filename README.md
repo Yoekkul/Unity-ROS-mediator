66b9fdf09820969aa73f74b3891690dc600c8fda
Yoekkul <tibaldo.chris@gmail.com>
2022-06-17 11:39:46 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Unity ROS Mediator
+
+
+## ROS nodes used
+* [grid_map](https://github.com/anybotics/grid_map/)
+* [ROS-TCP endpoint](https://github.com/Unity-Technologies/ROS-TCP-Endpoint)
+
+* [Unity robotics hub](https://github.com/Unity-Technologies/Unity-Robotics-Hub) <-contains URDF importer
+## Starting project
+`docker-compose up` -> (Rebuild from scratch) `docker-compose build --no-cache`
+
+`docker exec -it project_ros_hololens_mediator_1 bash`
---
+++ cat_ws/src/mediator/src/scale_grid_map_server.cpp
@@ -0,0 +1,35 @@
+#include <ros/ros.h>
+#include <grid_map_ros/grid_map_ros.hpp>
+#include <grid_map_cv/grid_map_cv.hpp>
+#include <mediator/GetScaledGridMap.h>
+
+grid_map::GridMap map;
+bool scale_map(mediator::GetScaledGridMap::Request &req, mediator::GetScaledGridMap::Response &res){
+    //TODO How can I get the proper map?
+    grid_map::GridMap modifiedMap;
+    grid_map::GridMapCvProcessing::changeResolution(map, modifiedMap, req.resolution);
+
+    grid_map_msgs::GridMap message;
+    grid_map::GridMapRosConverter::toMessage(modifiedMap, message);
+    res.map = message;
+
+    ROS_INFO("Scaled map to %lf [meters/cell]",req.resolution);
+    return true;
+}
+
+void map_update_callback(const grid_map_msgs::GridMap &message){
+    grid_map::GridMapRosConverter::fromMessage(message, map);
+    ROS_INFO("Received new grid map");
+}
+
+int main(int argc, char **argv){
+    ros::init(argc,argv, "scale_grid_map_server");
+    ros::NodeHandle n;
+
+    ros::Subscriber sub = n.subscribe("/grid_map_simple_demo/grid_map", 1000, map_update_callback);
+    ros::ServiceServer service = n.advertiseService("scale_grid_map",scale_map);
+    ROS_INFO("Ready to simplify map");
+    ros::spin();
+
+    return 0;
+}
